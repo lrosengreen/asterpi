6e9a339120194a3a23cda4f563362e8ec58152ef
Lars Rosengreen <lrosengreen@gmail.com>
2015-01-26 07:38:14 -0800

    timelapse camera for taking images.

---
+++ timelapse_camera.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+
+
+import datetime
+import io
+import os
+import shutil
+import sys
+import time
+
+import picamera
+
+
+_current_directory = os.path.dirname(os.path.abspath(__file__))
+_preview_directory =  "/mnt/ramdisk/previews"
+_picture_directory = _current_directory + "/pictures"
+_start_time = datetime.datetime.now()
+_timelapse_interval = 3 # how long to wait between taking pictures (in seconds)
+
+
+
+
+def free_space():
+    "Free disk space in gigabytes."
+    s = os.statvfs('/')
+    return (s.f_bavail * s.f_frsize) / 1.0e9
+
+
+def run():
+    if not os.path.exists(_preview_directory):
+        os.makedirs(_preview_directory)
+    if not os.path.exists(_picture_directory):
+        os.makedirs(_picture_directory)
+    counter = 0
+    with picamera.PiCamera() as camera:
+        camera.resolution = (2592, 1944) #2592, 1944
+        camera.vflip = True
+        camera.hflip = True
+        camera.start_preview()
+        time.sleep(5)
+        start_time = datetime.datetime.now()
+        next_time = start_time
+        try:
+            timestamp = datetime.datetime.now()
+            while free_space > 0.5:
+                next_time = next_time + datetime.timedelta(seconds=_timelapse_interval)
+                fname = os.path.join(_picture_directory, "{:06d}.jpg".format(counter))
+                camera.capture(fname)
+                if counter % 10 == 0:
+                    shutil.copy(fname, os.path.join(_preview_directory, "preview.jpg"))
+                wait_time = next_time - datetime.datetime.now()
+                wait_time = wait_time.seconds + wait_time.microseconds * 1e-6
+                print("time:{} images:{} wait:{}s".format(str(timestamp - start_time).split(".")[0], counter, wait_time))
+                if wait_time > _timelapse_interval: # happens when it takes too long to get picture
+                    wait_time = 0
+                time.sleep(wait_time)
+                counter += 1
+                timestamp = datetime.datetime.now()
+        finally:
+                camera.stop_preview()
+                camera.close()
+
+
+
+
+if __name__ == "__main__":
+    run()
