cac72d7fa094d49e5714ba9d0fb0934637febb2f
Lars Rosengreen <lrosengreen@gmail.com>
2015-02-03 06:30:13 -0800

    Stop recording when it is to dark to see anything.

--- video_camera.py
+++ video_camera.py
@@ -6,6 +6,7 @@ import datetime
 import io
 import os
 import sys
+import time
 
 import numpy
 import picamera
@@ -19,8 +20,9 @@ _movie_directory = _current_directory + "/movies"
 _resolution = (1920, 1080)
 _preview_resolution = (960, 540)
 _framerate = 4 #frames per second
-_start_time = datetime.datetime.now()
-
+_start_time = datetime.time(6)
+_end_time = datetime.time(7)
+_minimum_free_space = 0.5 #free space in GB
 
 def brightness(image):
     m = numpy.asarray(image)
@@ -38,34 +40,43 @@ def run():
         os.makedirs(_preview_directory)
     if not os.path.exists(_movie_directory):
         os.makedirs(_movie_directory)
-    counter = 1
-    with picamera.PiCamera() as camera:
-        camera.resolution = _resolution
-        camera.vflip = True
-        camera.hflip = True
-        camera.framerate = _framerate
-        camera.annotate_background = True
-        camera.start_preview()
-        try:
-            start_time = datetime.datetime.now()
-            camera.start_recording(os.path.join(_movie_directory,
-                "{}.h264".format(start_time.strftime("%Y%b%d_%H-%M-%S").lower())))
-            while free_space() > 0.5:
-                timestamp = datetime.datetime.now()
-                camera.annotate_text = timestamp.strftime("%Y%b%d %H:%M").lower()
-                camera.capture(os.path.join(_preview_directory,"preview.jpg"),
-                        resize=_preview_resolution,
-                        quality=30,
-                        use_video_port=True)
-                #print("\r{:78}".format(""), end="\r")
-                #print("\rrunning:{} previews:{} analog gain:{} digital gain:{} exposure speed:{}".format(str(timestamp - start_time).split(".")[0], counter, camera.analog_gain, camera.digital_gain, camera.exposure_speed), end="")
-                #sys.stdout.flush()
-                print("running:{} previews:{} analog gain:{} digital gain:{} exposure speed:{}".format(str(timestamp - start_time).split(".")[0], counter, camera.analog_gain, camera.digital_gain, camera.exposure_speed))
-                counter += 1
-                camera.wait_recording(60)
 
-        finally:
-            camera.stop_recording()
+    now = datetime.datetime.now().time()
+    while free_space() > _minimum_free_space:
+        now = datetime.datetime.now().time()
+        if now > _start_time and now < _end_time:
+            counter = 1
+            with picamera.PiCamera() as camera:
+                camera.resolution = _resolution
+                camera.vflip = True
+                camera.hflip = True
+                camera.framerate = _framerate
+                camera.annotate_background = True
+                try:
+                    start_time = datetime.datetime.now()
+                    camera.start_recording(os.path.join(_movie_directory,
+                        "{}.h264".format(start_time.strftime("%Y%b%d_%H-%M-%S").lower())))
+                    while free_space() > _minimum_free_space and now > _start_time and now < _end_time:
+                        timestamp = datetime.datetime.now()
+                        now = timestamp.time()
+                        camera.annotate_text = timestamp.strftime("%Y%b%d %H:%M").lower()
+                        camera.capture(os.path.join(_preview_directory,"preview.jpg"),
+                                resize=_preview_resolution,
+                                quality=30,
+                                use_video_port=True)
+                        print("\r{:78}".format(""), end="\r")
+                        print("\rrunning:{} previews:{}".format(str(timestamp - start_time).split(".")[0], counter), end="")
+                        sys.stdout.flush()
+                        counter += 1
+                        camera.wait_recording(60)
+                finally:
+                    camera.stop_recording()
+        else:
+            print("\r{:78}".format(""), end="\r")
+            print("\rwaiting until: {} ({})".format(str(_start_time), str(now).split(".")[0]), end="")
+            sys.stdout.flush()
+            time.sleep(20)
+
 
 if __name__ == "__main__":
     run()
